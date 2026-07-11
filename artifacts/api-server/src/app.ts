75fb811b114aa68549104b3dcd4babcecb80bf01
Deer-ux <affulbewuahdorcas2000@gmail.com>
2026-07-11 16:35:44 +0000

    Refactor import statement for pinoHttp

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,6 +1,6 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import { pinoHttp } from "pino-http";
+import pinoHttp from "pino-http";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
@@ -8,8 +8,8 @@ const app: Express = express();
 
 app.use(
   pinoHttp({
-    logger,
-    serializers: {
+  logger,
+  serializers: {
       req(req) {
         return {
           id: req.id,
