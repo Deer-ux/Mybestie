53759cc8e333bd142cb09e9f8d971cc80ac67eca
Deer-ux <affulbewuahdorcas2000@gmail.com>
2026-07-11 16:54:27 +0000

    Refactor pinoHttp import and serializers

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,6 +1,7 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
+import { pinoHttp } from "pino-http";
+import type { SerializedRequest, SerializedResponse } from "pino-std-serializers";
 import router from "./routes";
 import { logger } from "./lib/logger";
 
@@ -8,16 +9,17 @@ const app: Express = express();
 
 app.use(
   pinoHttp({
-  logger,
-  serializers: {
-      req(req) {
+    logger,
+    serializers: {
+      req(req: SerializedRequest) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+
+      res(res: SerializedResponse) {
         return {
           statusCode: res.statusCode,
         };
@@ -25,6 +27,7 @@ app.use(
     },
   }),
 );
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
